d275080a228debbc2c9a20765fa65ced8346b947
Loren Van Spronsen <loren.vanspronsen@gmail.com>
2010-12-18 00:04:05 -0800

    Added license header where missing

--- BACnet/ProtocolTypes/Enumerations/BACnetSilencedState.cs
+++ BACnet/ProtocolTypes/Enumerations/BACnetSilencedState.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using BACnet.Converters;
 
 namespace BACnet.ProtocolTypes
 {
-    [TypeConverter(typeof(EnumTypeConverter<BACnetSilencedState>))]
 	public enum BACnetSilencedState
 	{
 		Unsilenced = 0,
