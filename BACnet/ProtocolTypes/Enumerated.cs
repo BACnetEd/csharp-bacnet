a59c9ba9cc38702f690793c5e8cd4c9821d00153
Loren Van Spronsen <loren.vanspronsen@gmail.com>
2010-12-18 00:10:08 -0800

    Updated incorrect namespace in Enumerated.cs

--- BACnet/ProtocolTypes/Enumerated.cs
+++ BACnet/ProtocolTypes/Enumerated.cs
@@ -20,7 +20,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BACnet
+namespace BACnet.ProtocolTypes
 {
     /// <summary>
     /// Represents an untyped enumerated value
